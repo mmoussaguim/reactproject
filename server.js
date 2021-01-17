dfa060a3cc01fd45509cc1ecaa3f917450852784
Moussa Guimba Mamadou <g.guimba98@gmail.com>
2021-01-17 01:20:11 +0100

    Back end

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-//const connectDB = require('./config/db.js');
+const connectDB = require('./config/db');
 const app = express();
 
+// Connect Database
+connectDB();
+
+
+//Init Middleware
+
+app.use(express.json({ extended: false}));
 app.get('/', (req, res) => res.send(`API running`));
 
-// Connect Database
+//Define Routes
 
-//connectDB();
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profil', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5000;
 
